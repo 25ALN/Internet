6296ce148d6fae2963663f64a0a08f65fec7c32d
25ALN <3076331747@qq.com>
2025-04-14 21:30:13 +0800

    server

---
+++ client_test.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h> //sockaddr_in, htons() 等
+#include <arpa/inet.h> //inet_pton(), inet_ntoa() 等
+
+int main(){
+    int sock=socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in client_addr;
+    memset(&client_addr,0,sizeof(client_addr));
+    client_addr.sin_family=AF_INET;
+    client_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    client_addr.sin_port=htons(1234);
+    connect(sock,(struct sockaddr*)&client_addr,sizeof(client_addr));
+    char message[100];
+    read(sock,message,sizeof(message)-1);
+    printf("message have receved!\n");
+    printf("%s",message);
+    close(sock);
+    return 0;
+}
