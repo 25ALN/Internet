16a384c7d6bfabf8f0dece87deab5a7554224555
25ALN <3076331747@qq.com>
2025-05-06 16:29:41 +0800

    bc

--- client_test.cpp
+++ client_test.cpp
@@ -8,6 +8,7 @@
 #include <arpa/inet.h> //inet_pton(), inet_ntoa() µÈ
 
 int main(){
+    int mark=0;
     int sock=socket(AF_INET,SOCK_STREAM,0);
     struct sockaddr_in client_addr;
     memset(&client_addr,0,sizeof(client_addr));
@@ -28,7 +29,10 @@ int main(){
         printf("send fail\n");
     }
     while(1){
+        if(mark==0){
         printf("enter cycle\n");
+        }
+        mark=1;
         char buf[100]={};
         fgets(buf,sizeof(buf),stdin);
         buf[strcspn(buf, "\n")] = '\0';  // È¥µô»»ÐÐ·û
