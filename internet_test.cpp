fb72e83a2bfcea76eefdba760068c35c554cf03a
25ALN <3076331747@qq.com>
2025-05-08 22:38:11 +0800

    epoll lock

--- internet_test.cpp
+++ internet_test.cpp
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <mutex>
+#include <sys/epoll.h>
+#include <condition_variable>
+#include <thread>
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -70,43 +74,109 @@
 // }
 
 // getsockopt test 非阻塞connect
-int Connect(int fd,const sockaddr* clt,socklen_t clt_len,int mark){
-    int error_fd=0,flags,connect_fd;
-    socklen_t len;
-    flags=fcntl(fd,F_GETFL,0); //这一步获取当前文件表述符的状态，返回给了flags
-    fcntl(fd,F_SETFL,flags|O_NONBLOCK);
-    //设置当前文件法描述符为非阻塞的，使用|保留了之前文件描述符的状态，否则会被新的标志给覆盖
-    if(connect_fd=connect(fd,clt,len)<0){
-        if(error_fd!=EINPROGRESS){
-            //连接异常，即刻退出
-            exit(1);
-        }
-    }
-    //将套接字描述符和设置为非阻塞，后续用epoll进行完善
+// int Connect(int fd,const sockaddr* clt,socklen_t clt_len,int mark){
+//     int error_fd=0,flags,connect_fd;
+//     socklen_t len;
+//     flags=fcntl(fd,F_GETFL,0); //这一步获取当前文件表述符的状态，返回给了flags
+//     fcntl(fd,F_SETFL,flags|O_NONBLOCK);
+//     //设置当前文件法描述符为非阻塞的，使用|保留了之前文件描述符的状态，否则会被新的标志给覆盖
+//     if(connect_fd=connect(fd,clt,len)<0){
+//         if(error_fd!=EINPROGRESS){
+//             //连接异常，即刻退出
+//             exit(1);
+//         }
+//     }
+//     //将套接字描述符和设置为非阻塞，后续用epoll进行完善
 
-    int revel=getsockopt(fd,SOL_SOCKET,SO_ERROR,&error_fd,&len);
-    if(revel<0){
-        //异常退出
-        exit(1);
-    }else{
-        if(error_fd!=0){
-            std::cout<<"connect fail"<<std::endl;
-            exit(1);
-        }else{
-            std::cout<<"connect success"<<std::endl;
-        }
-    }
-}
+//     int revel=getsockopt(fd,SOL_SOCKET,SO_ERROR,&error_fd,&len);
+//     if(revel<0){
+//         //异常退出
+//         exit(1);
+//     }else{
+//         if(error_fd!=0){
+//             std::cout<<"connect fail"<<std::endl;
+//             exit(1);
+//         }else{
+//             std::cout<<"connect success"<<std::endl;
+//         }
+//     }
+// }
 
+// int main(){
+//     int sockfd=socket(AF_INET,SOCK_DGRAM,0);
+//     struct sockaddr_in clt;
+//     clt.sin_family=AF_INET;
+//     clt.sin_addr.s_addr=htonl(INADDR_ANY);
+//     clt.sin_port=htons(1234);
+//     if(Connect(sockfd,(struct sockaddr*)&clt,sizeof(clt),0)<0){
+//         std::cout<<"error"<<std::endl;
+//         exit(1);
+//     }
+//     return 0;
+// }
+
+//用锁保护accept，避免多个线程同时调用
+//使用一个主线程来监听accept函数，受到消息后传入线程池中进行工作
+// std::mutex accept_def;
+// std::condition_variable hx;
+
+
+// void accept_work(int fd,struct sockaddr *s,socklen_t s_len);
+// int main(){
+//     int server_fd=socket(AF_INET,SOCK_DGRAM,0);
+//     struct sockaddr_in ser;
+//     ser.sin_family=AF_INET;
+//     ser.sin_addr.s_addr=htonl(INADDR_ANY);
+//     ser.sin_port=htons(1234);
+//     bind(server_fd,(struct sockaddr*)&ser,sizeof(ser));
+
+//     listen(server_fd,5);
+//     //这里将会启动工作和线程
+//     std::thread accept_begin(accept_work,server_fd,(struct sockaddr*)&ser,sizeof(ser));
+//     return 0;
+// }
+
+// void accept_work(int fd,struct sockaddr *s,socklen_t s_len){
+//     while(true){
+//         if(accept(fd,s,&s_len)<0){
+//             continue;
+//         }
+//         {
+//             std::lock_guard<std::mutex> lock(accept_def);
+//             //这里将任务加入线程池开始执行任务
+//         }
+//         hx.notify_one(); //开始工作
+//     }
+// }
+
+// epoll例子，省略了服务器中具体的操作如bind,listen
 int main(){
-    int sockfd=socket(AF_INET,SOCK_DGRAM,0);
-    struct sockaddr_in clt;
-    clt.sin_family=AF_INET;
-    clt.sin_addr.s_addr=htonl(INADDR_ANY);
-    clt.sin_port=htons(1234);
-    if(Connect(sockfd,(struct sockaddr*)&clt,sizeof(clt),0)<0){
-        std::cout<<"error"<<std::endl;
+    int fd=epoll_create(1); //创建了一个epoll的实例
+    struct epoll_event ev,events[20];
+    int server_fd,ready=0,connect_fd; 
+
+    ev.data.fd=server_fd; //得到该事件的描述符
+    ev.events=EPOLLIN|EPOLLET; //设置该事件为可读，且切换为边缘触发模式
+
+    int reval=epoll_ctl(fd,EPOLL_CTL_ADD,server_fd,&ev); 
+    if(reval<0){
+        std::cout<<"epoll_ctl fail"<<std::endl;
         exit(1);
+    }
+
+    while(true){
+        ready=epoll_wait(fd,events,20,0); //得到已准备好的事件数量
+        for(int i=0;i<ready;i++){
+            if(server_fd==events[i].data.fd){ //当检测到有新的连接后，开始注册事件
+            //调用accept,并且将这个套接字设置为非阻塞模式
+            // 并且将这个新的事件加入到epoll的监听队列中去
+
+            ev.data.fd=connect_fd;
+            ev.events=EPOLLIN|EPOLLET;
+            epoll_ctl(fd,EPOLL_CTL_ADD,connect_fd,&ev);
+            }
+            // 下面就将处理客户端具体的消息内容
+        }
     }
     return 0;
 }
