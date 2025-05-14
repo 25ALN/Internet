55833ecddddb4ce096be1cd537a851fe740aa794
25ALN <3076331747@qq.com>
2025-05-14 22:22:17 +0800

    bc

--- ser.hpp
+++ ser.hpp
@@ -26,10 +26,11 @@ void ser_work(int fd,std::string ip);
 void clean_connect(int fd);
 
 void deal_new_connect(int ser_fd,int epoll_fd);
-void deal_client_data(int data_fd);
 void deal_pasv_data(int clinet_fd);
 void deal_list_data(int data_fd);
-void deal_STOR_data(int fd);
+void deal_STOR_data(std::shared_ptr<client_data> client,const std::string filename);
+void deal_RETR_data(std::shared_ptr<client_data> client,const std::string filename);
+void deal_client_data(int data_fd);
 
 int Recv(int fd,char *buf,int len,int flag);
 int Send(int fd,char *buf,int len,int flag);
